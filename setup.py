afd55cdfa06ddfd7b971c0ed033c7418db8223bb
Leo Developer <git@leodev.xyz>
2022-03-10 16:52:07 +0100

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_namespace_packages  # type: ignore
-
-
-def parse_requirements_file(path):
-    with open(path) as file:
-        lines = file.read().splitlines()
-    dependencies = (x.strip() for x in lines)
-    return [x for x in dependencies if x and not x.startswith("#")]
+from pathlib import Path
 
 
 setup(
@@ -15,7 +9,7 @@ setup(
     author="Leo Developer",
     author_email="git@leodev.xyz",
     description="ratelimit library for fastapi",
-    install_requires=parse_requirements_file("requirements.txt"),
+    install_requires=Path("requirements.txt").read_text().splitlines(),
     packages=find_namespace_packages(include=["cleaner_ratelimit*"]),
     package_data={"cleaner_ratelimit": ["py.typed"]},
 )
