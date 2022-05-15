028414b04715d13b77e73a3fa4d081fbc1f9ba52
Leo Developer <leodeveloper@protonmail.com>
2022-05-15 18:27:15 +0200

    fix warnings

--- tests/test_jail.py
+++ tests/test_jail.py
@@ -48,7 +48,7 @@ async def test_cloudflare_ip_access_rule_reporter():
     assert cfiparr.aclient.headers["x-auth-email"] == "test@test.cf"
     assert cfiparr.aclient.headers["x-auth-key"] == "0123456789"
 
-    cfiparr.aclient.post = post = mock.AsyncMock()
+    cfiparr.aclient.post = post = mock.AsyncMock(return_value=mock.Mock())
 
     await cfiparr(None, "1.2.3.0/24")
 
@@ -61,7 +61,7 @@ async def test_cloudflare_ip_access_rule_reporter():
             "note": "Automatic jail.",
         },
     }
-    post.assert_called_once_with(endpoint, json=json)
+    post.assert_awaited_once_with(endpoint, json=json)
 
 
 @pytest.mark.asyncio
@@ -69,7 +69,7 @@ async def test_cloudflare_ip_access_rule_reporter_zone_note():
     cfiparr = CloudflareIPAccessRuleReporter(
         "test@test.cf", "1234567890", "1234", "note"
     )
-    cfiparr.aclient.post = post = mock.AsyncMock()
+    cfiparr.aclient.post = post = mock.AsyncMock(return_value=mock.Mock())
 
     await cfiparr(None, "1.2.3.0/24")
 
@@ -78,4 +78,4 @@ async def test_cloudflare_ip_access_rule_reporter_zone_note():
         "mode": "block",
         "configuration": {"target": "ip_range", "value": "1.2.3.0/24", "note": "note"},
     }
-    post.assert_called_once_with(endpoint, json=json)
+    post.assert_awaited_once_with(endpoint, json=json)
