1ef1ac5213f99834ae91ef03eb51d8c8aa2e1be6
Leo Developer <leodeveloper@protonmail.com>
2022-05-15 18:29:08 +0200

    bump version and update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from pathlib import Path
 from setuptools import find_namespace_packages, setup  # type: ignore
 
 setup(
-    name="cleaner_ratelimit",
-    version="0.1.0",
-    url="https://github.com/cleaner-bot/cleaner-ratelimit",
+    name="slowerapi",
+    version="0.2.0",
+    url="https://github.com/cleaner-bot/slowerapi",
     author="Leo Developer",
     author_email="git@leodev.xyz",
     description="ratelimit library for fastapi",
     install_requires=Path("requirements.txt").read_text().splitlines(),
-    packages=find_namespace_packages(include=["cleaner_ratelimit*"]),
-    package_data={"cleaner_ratelimit": ["py.typed"]},
+    packages=find_namespace_packages(include=["slowerapi*"]),
+    package_data={"slowerapi": ["py.typed"]},
 )
